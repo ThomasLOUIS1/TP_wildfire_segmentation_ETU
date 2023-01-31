7591ba277d39e40cbd8da1956b7cc26f055e855d
root@LEAT199 <root@LEAT199>
2023-01-31 21:53:32 +0100

    Major update

--- README.md
+++ README.md
@@ -11,7 +11,17 @@
 <img src="images/predictions.jpg" ><br>
 </p>
 
+**Multi-spectral images** are images representing the same area in **multiple wavelength band**. They are very useful for wildfire segmentation because they can provide more information about the characteristics of the wildfire and the surrounding area than RGB images. 
 
+For example, near-infrared (NIR) bands can be used to identify vegetation, while shortwave infrared (SWIR) bands can be used to detect the presence of smoke. Additionally, using multiple wavelength bands can help to reduce the impact of atmospheric conditions such as clouds and haze on the image. This **can improve the accuracy of the segmentation task** and make it easier to **identify a wildfire** in an image.
+
+These images are stored in `.tif files`. **TIFF** is a widely-used **file format for images**. It is capable of storing images in a lossless format, meaning that **no data is lost when the image is compressed**. This makes it a popular choice for storing **high-quality images**, such as those used in professional photography, printing or satellite imagery.
+                                 
+Above are four **256x256** images :
+ - RGB images where each pixel is an 8-bit unsigned integer (uint8)
+ - The reconstructed images from the 3 spectral bands in uint8 (named "False Color")
+ - The binary masks of segmentation (named "GT Mask"). This is an "image" of the same size as the original reconstructed image but with values 0 and 1 (**Black/1 = no-fire; White/0 = fire**)
+ - Some predictions performed by a neural network (named "Prediction").
 
 
 <div id="top"></div>
@@ -26,7 +36,7 @@
     <li>
         <a href="#usage">Usage</a>
         <ul>
-            <li><a href="#demo_running">Demo running</a></li>
+            <li><a href="#Notebook_running">Notebook running</a></li>
             <li><a href="#data_structure">Data structure</a></li>
         </ul>
     </li>
@@ -43,14 +53,15 @@
 ## <div id="about-this-repository">1. About this repository 💼 </div>
 
 This git repository contains a Notebook to perform forest fire detection using deep learning. 
-The demo consists of loading the dataset, create several models trying to get good results and make the inference on some sample images.
+The Notebook consists in loading the dataset, creating several models trying to get good results and making the inference on some sample images.
 
 
-### <div id="packages"> This demo is built with </div>
+### <div id="packages"> This Notebook is built with </div>
 
 * [Anaconda](https://www.anaconda.com/products/distribution)
 * [Python](https://www.python.org/)
 * [Tensorflow](https://www.tensorflow.org/)
+* [scikit-learn](https://scikit-learn.org/)
 * [Matplotlib](https://matplotlib.org/)
 * [NumPy](https://numpy.org/)
 
@@ -62,7 +73,7 @@ The demo consists of loading the dataset, create several models trying to get go
 
 ## <div id="getting-Started"> 2.1 Getting Started (Colab)📚  </div>
 
-* Open this link in a new tab (CTRL+Click or Middle_click): [Run colab from this github](https://githubtocolab.com/ThomasLOUIS1/TP_wildfire_segmentation_ETU.git)
+* Open this link in a new tab (CTRL+Click or Middle_click): [Run colab from this github](https://githubtocolab.com/ThomasLOUIS1/Wildfire_segmentation.git)
 
 * Choose : 
     - Branche : main
@@ -94,9 +105,9 @@ Follow these instructions to setup your project.
 - Clone the repo and go to its root directory.
 
   ```sh
-  git clone "https://github.com/ThomasLOUIS1/TP_wildfire_segmentation_ETU.git"
+  git clone "https://github.com/ThomasLOUIS1/Wildfire_segmentation.git"
   
-  cd TP_wildfire_segmentation_ETU/
+  cd Wildfire_segmentation/
   ```
 
 - Install the rest of the environemnt:
@@ -109,38 +120,36 @@ Follow these instructions to setup your project.
 
 <!-- USAGE EXAMPLES -->
 
-## <div id="usage">3. Usage 📒 </div>
+## <div id="usage (linux)">3. Usage 📒 </div>
 
-### <div id="demo_running"> Demo running</div>
+### <div id="Notebook_running"> Notebook running</div>
 
-The jupyter nootebook is not installed in the Wildfire_segmentation env created by <a href="https://github.com/ThomasLOUIS1/Wildfire_segmentation/Wildfire_segmentation_env.yml">Wildfire_segmentation_env.yml </a> file.
+The jupyter librairy is not installed in the Wildfire_segmentation env created by <a href="https://github.com/ThomasLOUIS1/Wildfire_segmentation/Wildfire_segmentation_env.yml">Wildfire_segmentation_env.yml </a> file.
 
 🚨 To run the notebook **TP_wildfire.ipynb** you have to install **jupyter** package in your active (Wildfire_segmentation) environment
 ```bash
 conda install -c anaconda jupyter
 ```
 
-🏁 Run the notebook [**TP_wildfire.ipynb**](https://github.com/ThomasLOUIS1/TP_wildfire_segmentation_ETU/TP_wildfire_-_ETU.ipynb)
+🏁 Run the notebook [**TP_wildfire.ipynb**](https://github.com/ThomasLOUIS1/Wildfire_segmentation/TP_wildfire.ipynb)
 
 ```bash
-jupyter notebook TP_wildfire_-_ETU.ipynb
+jupyter notebook TP_wildfire.ipynb
 ```
 
 **✍ NOTES:**
 
-- [**TP_wildfire.ipynb**](https://github.com/ThomasLOUIS1/Wildfire_segmentation/TP_wildfire_segmentation_ETU.ipynb) will use some function declared in
+- [**TP_wildfire.ipynb**](https://github.com/ThomasLOUIS1/Wildfire_segmentation/TP_wildfire.ipynb) will use some function declared in
   utils.py and metrics_and_losses.py
   (<a href="#data_structure">bellow</a>).
 
 
-**✍ NOTES:**
-
 - The data folder only contains a .zip to extract to get the following dataset : 
-    - **false_color_full** folder contains images composed by [B12,B11,B04] (sentinel2 bands) used as the input for training. 
-    - **RGB** contains 6 images corresponding to RGB images used only for visualizing.
-    - **masks_full** folder contains binary masks (**value : 0=no-fire; value : 1=fire**) used as labels for training.
+    - **false_color** folder contains images composed by [B12,B11,B04] (sentinel2 bands) used as the input for training. 
+    - **RGB** contains 15 images corresponding to RGB images used only for visualizing.
+    - **masks** folder contains binary masks (**value : 0=no-fire; value : 1=fire**) used as labels for training.
 
-👉 For more information about the data please read this ([__Readme__](https://ciar.irt-saintexupery.com/index.php/s/3RLvFSak6Qt7NnB/download?path=%2Fwildifre_data_20m&files=ReadMe_20m.md))
+
 
 <p align="right">(<a href="#top">back to top</a>)</p>
 
@@ -165,11 +174,11 @@ Contains modified Copernicus Sentinel data [2016-2020] for Sentinel data
 ## <div id="contact">5. Contact 📭</div>
 
 * [Thomas LOUIS 📧](mailto:thomas.louis@irt-saintexupery.com)
-* [Alain PEGATOQUET 📧] (mailto:(mailto:alain.pegatoquet@unice.fr))
+* [Alain PEGATOQUET 📧](mailto:alain.pegatoquet@univ-cotedazur.fr)
 
 
 
-Project Link: [https://github.com/ThomasLOUIS1/TP_wildfire_segmentation_ETU](https://github.com/ThomasLOUIS1/TP_wildfire_segmentation_ETU)
+Project Link: [https://github.com/ThomasLOUIS1/Wildfire_segmentation](https://github.com/ThomasLOUIS1/Wildfire_segmentation)
 
 <p align="right">(<a href="#top">back to top</a>)</p>
 
--- utils.py
+++ utils.py
@@ -5,55 +5,115 @@ import PIL
 from PIL import Image
 
 def display_confusion_matrix(Y_pred, Y_true):
-
-    FP = len(np.where(Y_pred - Y_true  == 1)[0])
-    FN = len(np.where(Y_pred - Y_true  == -1)[0])
+    
+    # Calculating the number of False Positives
+    FP = len(np.where(Y_pred - Y_true == 1)[0])
+    # Calculating the number of False Negatives
+    FN = len(np.where(Y_pred - Y_true == -1)[0])
+    # Calculating the number of True Positives
     TP = len(np.where(Y_pred + Y_true ==2)[0])
+    # Calculating the number of True Negatives
     TN = len(np.where(Y_pred + Y_true == 0)[0])
+    # Creating the confusion matrix
     cmat = [[TP, FN], [FP, TN]]
-
+    
+    # Plotting the heatmap of the confusion matrix with number of pixel
     plt.figure(figsize = (6,6))
-    sns.heatmap(cmat/np.sum(cmat), cmap="Reds", annot=True, fmt = '.2%', square=1,   linewidth=2.)
+    sns.heatmap(cmat, cmap="Reds", annot=True, fmt = 'd', square=1,   linewidth=2., xticklabels = ["Fire","No_Fire"], yticklabels = ["Fire","No_Fire"])
+    plt.title("Pixel number confusion matrix")
+    plt.xlabel("predictions")
+    plt.ylabel("real values")
+    plt.show()
+
+    # Plotting the heatmap of the confusion matrix with pixel percentage
+    plt.figure(figsize = (6,6))
+    sns.heatmap(cmat/np.sum(cmat), cmap="Reds", annot=True, fmt = '.02%', square=1,   linewidth=2., xticklabels = ["Fire","No_Fire"], yticklabels = ["Fire","No_Fire"])
+    plt.title("Pixel percentage confusion matrix")
     plt.xlabel("predictions")
     plt.ylabel("real values")
     plt.show()
 
 
-def load_masks(paths = None, img_size = None):
+def print_TPFNFPTN(Y_pred, Y_true):
+    
+    # Calculating the number of False Positives
+    FP = len(np.where(Y_pred - Y_true == 1)[0])
+    # Calculating the number of False Negatives
+    FN = len(np.where(Y_pred - Y_true == -1)[0])
+    # Calculating the number of True Positives
+    TP = len(np.where(Y_pred + Y_true ==2)[0])
+    # Calculating the number of True Negatives
+    TN = len(np.where(Y_pred + Y_true == 0)[0])
+
+    # print values
+    print("TP : {} FN : {} FP : {} TN : {} ".format(TP, FN ,FP, TN))
+def load_masks(paths=None, img_size=None):
+    """
+    Load image masks from file paths
+    """
+    # Initialize an array of zeros with the shape (number of masks, img_size, 1)
     y = np.zeros((len(paths),) + img_size + (1,), dtype="float32")
-    for i, (targ_path)  in enumerate(paths):
+    
+    # Loop through each mask file path
+    for i, (targ_path) in enumerate(paths):
+        # Load the mask image from the file path
         out = np.array(Image.open(targ_path))
+        # Add an extra dimension to the mask image
         out = np.expand_dims(out, 2)
+        # Set the corresponding position in the array to the mask image
         y[i] = out
+    # Return the loaded masks
     return y
 
-def print_score(score) :
+def print_score(score):
+    """
+    Print the performance score with a label
+    """
+    # List of score names
     score_names = ["loss", "Accuracy", "Recall", "Precision", "f1_score"]
-    
-    score_and_names = zip(score_names,score)
-    
-    for i,j in score_and_names :
-        print("|{} : {}".format(i,j), end='|')
+    # Zip the score names and values
+    score_and_names = zip(score_names, score)
+    # Loop through each score and name pair
+    for i, j in score_and_names:
+        # Print the score name and value with a % format
+        print("|{}: {:.02%}".format(i, j), end='|\t')
 
-def display_matrix(matrix,title_list,path_save="image"):
+def display_matrix(matrix, title_list, path_save="image"):
+    """
+    Display a matrix of images with titles
+    """
+    # Number of columns in the matrix
     columns = len(matrix[0])
+    # Number of rows in the matrix
     rows = len(matrix)
-    fig=plt.figure(figsize=(5*columns,5*rows))
-
-
-    for j in range(0,rows):
-        for i in range(0,columns):
-            ax=fig.add_subplot(rows,columns,(columns*j)+i+1)
+    # Create a figure with the specified number of columns and rows
+    fig = plt.figure(figsize=(5 * columns, 5 * rows))
+    # Loop through each row and column in the matrix
+    for j in range(0, rows):
+        for i in range(0, columns):
+            # Add a subplot for the current image
+            ax = fig.add_subplot(rows, columns, (columns * j) + i + 1)
+            # Set the title for the first row
             if j == 0:
-                ax.set_title(title_list[i],c='black')
+                ax.set_title(title_list[i], c='black')
+            # Set the aspect ratio to be equal
             ax.set_aspect('equal')
+            # Display the image
             plt.imshow(matrix[j][i])
+            # Turn off the axis
             plt.axis('off')
+    # Adjust the space between subplots
     plt.subplots_adjust(wspace=0.05, hspace=0.05)
+    # Show the plot
     plt.show()
-    #fig.savefig(path_save+'.jpg',bbox_inches='tight', dpi=150)
-    
+    # Uncomment the line below to save the plot to a file
+    # fig.savefig(path_save + '.jpg', bbox_inches='tight', dpi=150)
+
 def transform_predicted_mask(pred_mask, threshhold=0.5):
+    """
+    Transform a predicted mask to binary format based on the threshhold value
+    """
+    # Set all values above the threshhold
     pred_mask[pred_mask >= threshhold] = 1.0
     pred_mask[pred_mask < threshhold] = 0.0
     return pred_mask.astype(np.uint8)
@@ -63,21 +123,56 @@ def predict(model, image, threshhold=0.5):
     """
     use the model to predict a mask from image as input
     """
-    pred_mask = model.predict(image)
+    pred_mask = model.predict(image, verbose = 0)
     return transform_predicted_mask(pred_mask, threshhold=threshhold)
 
 
-def write_triband_and_target_from_paths(paths = None, folder = None):
-    import shutil
-    for i, (in_path, targ_path)  in enumerate(paths):
-        shutil.copy(in_path, "/hdd-raid0/home_server/thomasl/Vacations/MCIA/Wildfire_segmentation/data/false_color_new/"+folder)
-        shutil.copy(targ_path, "/hdd-raid0/home_server/thomasl/Vacations/MCIA/Wildfire_segmentation/data/masks_new/"+folder)
 
+# A function that display firsts n_range tri-bands, masks and predictions
+def display_sample_prediction(model, triband_paths, mask_paths, n_range = 3): 
+  # Pass through n_range firsts images 
+  i = 0
+  for id in range(len(triband_paths)):
+    # Read the mask data for the current iteration
+    image_masks = Image.open(mask_paths[id])
+    # Check if there's at least one "Fire" pixel and if the number of images displayed is less than n_range
+
+    if np.max(image_masks) > 0 and i < n_range:
+          
+      # Read and normalize triband
+      n_triband = np.array(Image.open(triband_paths[id]))/255.0
+      
+      # Add an extra dimension to the triband data for compatibility with the model input
+      tribands = np.expand_dims(n_triband, 0) # Same as np.reshape(tribands, (1,256,256,3))
+
+      # print id and path of the associated Tri-bands path
+      print("Image n {}, path : {}".format(id, triband_paths[id]))
+      
+      predicted_mask = predict(model, tribands)
+      predicted_mask = np.squeeze(predicted_mask)
+
+      matrix=[[n_triband, image_masks, predicted_mask]] # [[Tribands,mask,predict]]
+
+      display_matrix(matrix ,title_list=['Tri-bands', 'GT Mask', 'Predicted'])
+      
+      # Increment counter for number of images displayed
+      i += 1
+    
+    # Break out of loop if n_range images have been displayed
+    if i == n_range:
+        break
 
 
 # Used to create the TP
-
 '''
+def write_triband_and_target_from_paths(paths = None, folder = None):
+    import shutil
+    for i, (in_path, targ_path) in enumerate(paths):
+    # copy the input image to false_color_new folder with the specified folder name
+    shutil.copy(in_path, "/hdd-raid0/home_server/thomasl/Vacations/MCIA/Wildfire_segmentation/data/false_color_new/"+folder)
+    # copy the target mask to masks_new folder with the specified folder name
+    shutil.copy(targ_path, "/hdd-raid0/home_server/thomasl/Vacations/MCIA/Wildfire_segmentation/data/masks_new/"+folder)
+
 # TO DO Choice the right proportion for the TP
 import random
 
